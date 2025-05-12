39c6c6faa293547c10e2871e844978ba500f7b5c
Davit Mutafyan <davitmutafyan@gmail.com>
2025-05-12 13:21:45 +0400

    fix dual index js problem, update package json

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.end('Hello from CI/CD app!');
-});
-
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
